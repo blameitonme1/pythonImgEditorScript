495ec2efd0f3285876e9160499c9f27caeb28b17
blameitonme1 <2741122036@qq.com>
2023-10-05 21:45:22 +0800

    只完成了一个最基础的功能，剩下的可以根据需求来

--- photeEditor.py
+++ photeEditor.py
@@ -1,6 +1,13 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 path = './imgs'
+pathout = "/editedImgs"
 for filename in os.listdir(path):
-    img = Image.open(f"{path} / {filename}")
-    
+    # 注意别加空格
+    img = Image.open(f"{path}/{filename}")
+    # 轮廓
+    edit = img.filter(ImageFilter.CONTOUR)
+    # 干净的名字
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f'.{pathout}/{clean_name}_edited.jpg')
+
